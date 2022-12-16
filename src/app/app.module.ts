715249ed689d80b81af9b10d45e7567639ae2b56
gang wang <harry74323@gmail.com>
2022-12-16 20:55:26 +1100

    basic theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {MaterialExampleModule} from '../material.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialExampleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
